61b98db7ffd7da94fd90e91763f9a8e392f7d703
S17-V <1583355176@qq.com>
2025-11-01 12:21:53 +0800

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,21 +1,46 @@
 #include<stdio.h>
-int n, h, m;
-int main()
-{
-	printf("enter integer minutes:\n");
-	while (1) {
-		int count = scanf_s("%d",&n);
-		if (count == 1) {
-			break;
-		}
-		else {
-			int c;
-			while ((c = getchar()) != '\n' && c != EOF) {}
-			printf("input error");
-		}
-	}
-	h = n / 60;
-	m = n % 60;
-	printf("you entered: %d minutes are %d hours and %d minutes\n", n, h, m);
-	return 0;
+#include <string.h>
+int main() {
+    int a, r;
+    printf("enter a integer numbers between 0 and 9999\n");
+
+    while (1) {
+        int count = scanf_s("%d", &a);
+        if ((count == 0) || (count == EOF))
+        {
+            printf("Input error.\n");
+            int r;
+            while ((r = getchar()) != '\n' && r != EOF) {}
+            continue;
+        }
+        else {
+            int digits = 0;
+            r = a;
+            if (r == 0) {
+                digits = 1;
+                printf(" the number of digits is %d\n", digits);
+            }
+            else {
+                while (r != 0) {
+                    r /= 10;
+                    digits++;
+                }
+            }
+            printf(" the number of digits is %d\n", digits);
+
+        }
+        char buf[5];
+        snprintf(buf, sizeof(buf), "%d", a);
+        int len = (int)strlen(buf);
+        for (int i = 0, j = len - 1; i < j; i++, j--) {
+            char tmp = buf[i];
+            buf[i] = buf[j];
+            buf[j] = tmp;
+        }
+        printf("the number is %s\n", buf);
+        break;
+    }
+    return 0;
 }
+
+
